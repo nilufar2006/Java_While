5eacbb1b754fc8c2d54f360c10bc84baf8138603
nilufar2006 <110012423+nilufar2006@users.noreply.github.com>
2022-07-30 11:43:29 +0500

    Add files via upload

---
+++ nilufar1515/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        int a = 1;
+        int y = 0;
+        int k = 1;
+
+
+        while (a <= 10) {
+            if (a % 2 == 1) {
+
+                System.out.println(a);
+            }
+
+            a++;
+        }
+    }
+}
